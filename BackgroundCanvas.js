d17e63a9e4952739f6fe9283f72fb00cabb6ebf1
augunautics <thomas.a.augustine@gmail.com>
2024-08-28 12:30:32 -0700

    refactor

---
+++ BackgroundCanvas.js
@@ -0,0 +1,16 @@
+export default class BackgroundCanvas {
+    constructor(width, height, bgColor, positionTop, positionLeft) {
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.canvas.style.position = 'absolute';
+        this.canvas.style.top = `${positionTop}px`;
+        this.canvas.style.left = `${positionLeft}px`;
+
+        this.ctx = this.canvas.getContext('2d');
+        this.ctx.fillStyle = bgColor;
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        document.body.appendChild(this.canvas);
+    }
+}
