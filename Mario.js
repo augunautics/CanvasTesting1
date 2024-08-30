9383c44c9d7cfb906b4b7b05c8c547b4011c58f2
augunautics <thomas.a.augustine@gmail.com>
2024-08-30 10:28:22 -0700

    done

--- Mario.js
+++ Mario.js
@@ -6,17 +6,4 @@ export default class Mario extends Entity {
         this.sourceX = 0;
         this.sourceY = 8;
     }
-
-    update(sourceX, sourceY) {
-        // Custom updates for Mario, if needed
-        super.update(sourceX, sourceY);
-    }
-
-    start() {
-        const updateLoop = () => {
-            this.update(this.sourceX, this.sourceY);
-            requestAnimationFrame(updateLoop);
-        };
-        updateLoop();
-    }
 }
